31c25b1581990c1efe48b487a66a359d104a62e3
Joseph Morain <aelphaeis@live.com>
2016-12-28 13:25:24 -0500

    Cleaned up driver

--- src/main/java/driver/Program.java
+++ src/main/java/driver/Program.java
@@ -3,20 +3,27 @@ package driver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Stack;
 
 import logos.DeleteStatementResolver;
 import structures.TreeNode;
 import pojo.TableDependencyInfo;
-import utilities.MySqlUtils;
 
 public class Program {
 
 	public static void main(String[] args) throws Exception {
+		printDeleteStatements();
+	}
+	
+	public static void printDeleteStatements() throws SQLException{
+		//You need to specify schema table and primary key for specified table.
+		String schema = "";
+		String table = "";
+		String tablePK = "";
+		Connection conn = getConnection();
 		
 		long start = System.currentTimeMillis();
 		TreeNode<TableDependencyInfo> dependencies;
-		dependencies = DeleteStatementResolver.resolveDependencies(getConnection(), "local_prime", "Company", "idCompany");
+		dependencies = DeleteStatementResolver.resolveDependencies(conn, schema, table, tablePK);
 		DeleteStatementResolver resolver = new DeleteStatementResolver();
 		dependencies.transverseNodes(resolver);
 		System.out.println(resolver);
@@ -26,41 +33,7 @@ public class Program {
 		String msg = "Resolved dependencies in %s milliseconds";
 		System.out.println(String.format(msg, duration));
 	}
-
-
-	public static void ShowHierarchy(String table, String schema, String idColumn) throws SQLException {
-		TableDependencyInfo root = new TableDependencyInfo();
-		TreeNode<TableDependencyInfo> info = MySqlUtils.listChildHierarchy(getConnection(), schema, table);
-
-		root.setReferencedColumnName(idColumn);
-		root.setReferencedTableName(table);
-		root.setConstraintSchema(schema);
-		root.setColumnName(idColumn);
-		root.setTableName(table);
-		info.setValue(root);
-
-		Stack<String> out = new Stack<String>();
-
-//		info.transverse(new Callable() {
-//			@SuppressWarnings("unchecked")
-//			@Override
-//			public void function(Object... args) {
-//				TreeNode<TableDependencyInfo> n = (TreeNode<TableDependencyInfo>) args[1];
-//				StringBuilder sb = new StringBuilder("");
-//				for(int i = n.getLevel(); i > 1; i--){
-//					sb.append("   ");
-//				}
-//				sb.append("|--- " + n.getValue().getTableName());
-//				out.push(sb.toString());
-//			}
-//
-//		});
-		
-		for(String s : out){
-			System.out.println(s);
-		}
-	}
-
+	
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(
 				"jdbc:mysql://localhost:3306/prod_prime", 
