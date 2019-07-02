48607fc740c08f455981ab7e9e0bdf971ca713a4
Joseph Morain <morain@echoworx.com>
2019-07-02 12:31:03 -0400

    logger for ping

--- src/main/java/logos/Ping.java
+++ src/main/java/logos/Ping.java
@@ -5,20 +5,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Objects;
 
+import org.apache.log4j.Logger;
+
 public class Ping implements AutoCloseable {
+	
+	private static final Logger logger = Logger.getLogger(Ping.class);
+	
 	private final Connection connection;
 	
 	public Ping(Connection c) {
 		this.connection = Objects.requireNonNull(c);
 	}
 	
-
 	public void ping() throws SQLException {
 		try(Statement s = connection.createStatement()){
 			s.executeQuery("select 1 from dual");
 		}
+		logger.info("successfully pinged server");
 	}
-
 
 	@Override
 	public void close() throws Exception {
