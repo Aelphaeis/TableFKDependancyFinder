1c12e0a22b11854872b89d436da97b32f5f4f479
Joseph Morain <morain@echoworx.com>
2019-07-02 17:40:39 -0400

    implement list table.

--- src/main/java/com/cruat/tools/aide/database/driver/ListTables.java
+++ src/main/java/com/cruat/tools/aide/database/driver/ListTables.java
@@ -8,7 +8,6 @@ import org.apache.log4j.Logger;
 import com.cruat.tools.aide.database.behaviors.BehaviorFactory;
 import com.cruat.tools.aide.database.behaviors.impl.SchemaTableResolver;
 import com.cruat.tools.aide.database.utilities.DatabaseSettings;
-import com.cruat.tools.aide.database.utilities.Databases;
 import com.cruat.tools.aide.database.utilities.QuerySettings;
 
 public class ListTables {
@@ -16,21 +15,13 @@ public class ListTables {
 	private static final Logger logger = Logger.getLogger(ListTables.class);
 	public static final String SCHEMA = QuerySettings.SCHEMA.validate();
 	
-	public static void main(String ... args) {
-	
-	}
-	
-	public static void a() {
-		try(Connection connection  = DatabaseSettings.getConnection()){
-			logger.info(Databases.DBMS.resolve(connection));
-			SchemaTableResolver resolver = new SchemaTableResolver();
-			resolver.getAllTables(connection, SCHEMA).forEach(logger::info);
+	public static void main(String... args) {
+		BehaviorFactory factory = new BehaviorFactory();
+		SchemaTableResolver r = factory.getBehavior(SchemaTableResolver.class);
+		try (Connection connection = DatabaseSettings.getConnection()) {
+			r.getAllTables(connection, SCHEMA).forEach(logger::info);
 		} catch (SQLException e) {
 			logger.error("Error occured with connection", e);
 		}
-	}
-	public static void b() {
-		BehaviorFactory factory = new BehaviorFactory();
-		factory.getBehavior(SchemaTableResolver.class);
 	}
 }
