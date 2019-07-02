8bc7cda1b32eb7f8002e157047f1f8523e0593fb
Joseph Morain <morain@echoworx.com>
2019-07-02 12:28:40 -0400

    Exceptions from system

---
+++ src/main/java/exceptions/FinderException.java
@@ -0,0 +1,21 @@
+package exceptions;
+
+public class FinderException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public FinderException() {
+		super();
+	}
+	
+	public FinderException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public FinderException(String message) {
+		super(message);
+	}
+	
+	public FinderException(Throwable cause) {
+		super(cause);
+	}
+}
---
+++ src/main/java/exceptions/FinderRuntimeException.java
@@ -0,0 +1,22 @@
+package exceptions;
+
+public class FinderRuntimeException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public FinderRuntimeException() {
+		super();
+	}
+	
+	public FinderRuntimeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public FinderRuntimeException(String message) {
+		super(message);
+	}
+	
+	public FinderRuntimeException(Throwable cause) {
+		super(cause);
+	}
+}
