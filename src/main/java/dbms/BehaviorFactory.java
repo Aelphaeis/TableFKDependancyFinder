004ba3282750195fdb64df2fc9369608e4556ea6
Joseph Morain <morain@echoworx.com>
2019-07-02 16:07:52 -0400

    overload for getbehavior in behavior factory using default settings.

--- src/main/java/dbms/BehaviorFactory.java
+++ src/main/java/dbms/BehaviorFactory.java
@@ -1,10 +1,14 @@
 package dbms;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import exceptions.FinderRuntimeException;
+import utilities.DatabaseSettings;
+import utilities.Databases;
 import utilities.Databases.DBMS;
 
 public class BehaviorFactory {
@@ -13,22 +17,28 @@ public class BehaviorFactory {
 		BMAP = new BehaviorMap<>();
 	}
 	
+	public <T extends Behavior> T getBehavior(Class<T> type) {
+		try (Connection connection = DatabaseSettings.getConnection()) {
+			return getBehavior(Databases.DBMS.resolve(connection), type);
+		} catch (SQLException e) {
+			throw new FinderRuntimeException(e);
+		}
+	}
+	
 	public <T extends Behavior> T getBehavior(DBMS dbms, Class<T> type) {
-		List<Behavior> behaviors = BMAP.getOrDefault(type, new ArrayList<>());	
+		List<Behavior> behaviors = BMAP.getOrDefault(type, new ArrayList<>());
 		List<Behavior> result = behaviors.stream()
-			.filter(p -> dbms.equals(p.getVendor()))
-			.filter(p -> type.isInstance(behaviors))
-			.collect(Collectors.toList());
+				.filter(p -> dbms.equals(p.getVendor()))
+				.filter(p -> type.isInstance(behaviors))
+				.collect(Collectors.toList());
 		
-		if(result.isEmpty()) {
+		if (result.isEmpty()) {
 			String err = "resource of type[%s] and dbms[%s] not found";
 			throw new FinderRuntimeException(String.format(err, type, dbms));
-		}
-		else if(result.size() > 2) {
-			String err = "too many found"; //TODO better err msg
+		} else if (result.size() > 2) {
+			String err = "too many found"; // TODO better err msg
 			throw new FinderRuntimeException(String.format(err));
-		}
-		else {
+		} else {
 			return type.cast(result.get(0));
 		}
 	}
